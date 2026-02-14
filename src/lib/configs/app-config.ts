210f55c24197671645f7cd2ba082182361596fac
Amit Dubey <36706750+JustSP@users.noreply.github.com>
2026-02-14 18:58:46 +0000

    Updated

--- src/lib/configs/app-config.ts
+++ src/lib/configs/app-config.ts
@@ -3,12 +3,15 @@ import express, { Request, Response, NextFunction, Express } from "express";
 export const prepareToRun = (app: Express) => {
   app.use(express.json({ limit: "10mb" }));
   app.use((req: Request, res: Response, next: NextFunction) => {
-    const allowedOrigins = ["http://localhost:3000"];
-    const origin = req.headers.origin ?? "";
+    // const allowedOrigins = ["http://localhost:3000"];
 
-    if (allowedOrigins.includes(origin) || origin.includes("postman")) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-    }
+    // const origin = req.headers.origin ?? "";
+
+    // if (allowedOrigins.includes(origin) || origin.includes("postman")) {
+    //   res.setHeader("Access-Control-Allow-Origin", origin);
+    // }
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, PATCH, PUT, OPTIONS, DELETE",
